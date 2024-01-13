6dc368eec875e9b4ed0b3b887eb819ff3e6cea21
Taimul <taimul.haque@gmail.com>
2024-01-13 13:00:15 +0600

    edit category

--- controllers/category.js
+++ controllers/category.js
@@ -7,11 +7,15 @@ exports.addCategory = async (req, res) => {
     if (!categoryName) {
       return res.status(400).json({ error: "Category name is required" });
     }
-    const existingCategory = await Category.findOne({ categoryName });
+    const existingCategory = await Category.findOne({
+      categoryName: { $regex: new RegExp(categoryName, "i") },
+    });
+
     if (existingCategory) {
-      return res
-        .status(400)
-        .json({ error: "Category with this name already exists" });
+      return res.status(400).json({
+        success: false,
+        error: "Category with this name already exists",
+      });
     }
     const newCategory = new Category({ categoryName });
     const savedCategory = await newCategory.save();
@@ -25,6 +29,7 @@ exports.addCategory = async (req, res) => {
 
 exports.getAllCategories = async (req, res) => {
   try {
+    // await Category.deleteMany();
     const categories = await Category.find();
     return res.status(200).json(categories);
   } catch (error) {
--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { addCategory, getAllCategories } = require("../controllers/category");
+const {
+  addCategory,
+  getAllCategories,
+  editCategory,
+} = require("../controllers/category");
 
 router.post("/add-category", addCategory);
 router.get("/get-all-category", getAllCategories);
+router.put("/edit-category", editCategory);
 
 module.exports = router;
